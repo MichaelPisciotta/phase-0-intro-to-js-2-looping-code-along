5ada2533674f3a009d015441cf5a8fe5b46774b4
MichaelPisciotta <mfp56@miami.edu>
2021-06-20 13:48:30 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solutions in this file
+
+const people = [ 'Lisa', 'Kaitlin', 'Jan' ];
+
+function writeCards(people) {
+  for (let i = 0; i < people.length; i++) {
+    console.log(`Thank you, ${people[i]} for the wonderful birthday gift!`);
+    debugger;
+  }
+
+}
+writeCards();
+
+
+
+
+
+
+
+
+function countDown() {
+    let countdown = 10;
+    while (countdown >= 0) {
+      console.log(countdown);
+      countdown--;
+      debugger;
+    } 
+}
+ countDown(10);
+
+
